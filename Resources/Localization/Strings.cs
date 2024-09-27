0d95524ce041bc8c3a0dae251ff395bcebf29990
Britta <kappe619@gmail.com>
2024-09-27 14:14:21 +0200

    FEAT: Basics for resx files

---
+++ Resources/Localization/Strings.cs
@@ -0,0 +1,21 @@
+using System.Globalization;
+using System.Resources;
+
+public static class Strings
+
+{
+
+
+
+    public static string? testString = "test the resource";
+    private static readonly ResourceManager resourceManager = new ResourceManager("FoodLog/Resources.Localization.Strings", typeof(Strings).Assembly);
+    #region Resource Manager
+    //FIX: Implement correct resx handling
+    // private static readonly ResourceManager resourceManager = new ResourceManager("Resources.Localization.Strings", typeof(Strings).Assembly);
+    // private static readonly ResourceManager resourceManager = new ResourceManager("FoodLog.Resources.Localization.Strings", typeof(Strings).Assembly);
+
+    // public static string? MyText => resourceManager.GetString("MyText", CultureInfo.CurrentUICulture);
+    // public static string MyButtonText2 => resourceManager.GetString("MyButtonText", new CultureInfo("de-DE"));
+    #endregion
+}
+
