062a43921eda74fc4d5a16bf1151165bf3f20595
Britta <kappe619@gmail.com>
2024-09-28 10:07:53 +0200

    ADD: FoodItem class and example json data

---
+++ Models/FoodItem.cs
@@ -0,0 +1,9 @@
+namespace FoodLog.Models
+{
+    public class FoodItem
+    {
+        public string Name { get; set; }
+        public int Calories { get; set; }
+        public double Protein { get; set; }
+    }
+}
