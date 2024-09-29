4cb722aedb012bcaa547468efe709721dde12fe4
Britta <kappe619@gmail.com>
2024-09-29 14:10:22 +0200

    ADD Microcharts with GraphView and ViewModel for example chart data

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microcharts.Maui;
 
 namespace FoodLog;
 
@@ -7,6 +8,7 @@ public static class MauiProgram
 	public static MauiApp CreateMauiApp()
 	{
 		var builder = MauiApp.CreateBuilder();
+		builder.UseMicrocharts();
 		builder
 			.UseMauiApp<App>()
 			.ConfigureFonts(fonts =>
