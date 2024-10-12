4390af086b054af1d7d5f3cc7f4cb610a673dd46
Britta <kappe619@gmail.com>
2024-10-12 11:22:58 +0200

    FEAT: integrate Syncfusion with secure license handling
    - Added Syncfusion package for enhanced UI components
    - Implemented license key loading from .env file
    - Updated .gitignore to exclude .env file, securing license key

--- FoodLog.MAUI/App.xaml.cs
+++ FoodLog.MAUI/App.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using FoodLog.MAUI.Services;
 using FoodLog.Shared.Interfaces;
+using DotNetEnv;
+using Syncfusion.Licensing;
+using FoodLog.Shared.Services;
 
 namespace FoodLog.MAUI
 {
@@ -8,6 +11,16 @@ namespace FoodLog.MAUI
         public App()
         {
             InitializeComponent();
+
+            // Load environment variables from .env file
+            DotNetEnv.Env.Load(FilePathService.EnvFilePath());
+
+            // Retrieve and register the Syncfusion license key
+            var licenseKey = Environment.GetEnvironmentVariable("SYNCFUSION_LICENSE_KEY");
+            if (!string.IsNullOrEmpty(licenseKey))
+            {
+                SyncfusionLicenseProvider.RegisterLicense(licenseKey);
+            }
 
             MainPage = new AppShell();
             //builder.Services.AddSingleton<IFileService, MauiFileService>();
--- FoodLog.WPF/App.xaml.cs
+++ FoodLog.WPF/App.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System.Configuration;
+﻿using Syncfusion.Licensing;
+using System.Configuration;
 using System.Data;
 using System.Windows;
+using DotNetEnv;
+using System.IO;
+using FoodLog.Shared.Services;
 
 namespace FoodLog.WPF
 {
@@ -9,6 +13,26 @@ namespace FoodLog.WPF
     /// </summary>
     public partial class App : Application
     {
-    }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            DotNetEnv.Env.Load(FilePathService.EnvFilePath());
+
+            // Retrieve and register the Syncfusion license key
+            var licenseKey = Environment.GetEnvironmentVariable("SYNCFUSION_LICENSE_KEY");
+            if (!string.IsNullOrEmpty(licenseKey))
+            {
+                SyncfusionLicenseProvider.RegisterLicense(licenseKey);
+            }
+            else
+            {
+                MessageBox.Show("Syncfusion license key not found.");
+            }
+        }
+
+
+}
 
 }
